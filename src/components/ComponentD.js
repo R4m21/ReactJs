0a6771deb81beee7b9bd36a84d530a76c87218ad
maniram chuahan <cr4m21@gmail.com>
2021-11-01 23:15:39 +0530

    context part3 can access username ComponentD contextType as a UserContext

--- src/components/ComponentD.js
+++ src/components/ComponentD.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import ComponentF from './ComponentF'
+import UserContext from './userContext'
 
 class ComponentD extends Component {
     render() {
         return (
             <>
+            Component D context {this.context}
                 <ComponentF/>
             </>
         )
     }
 }
+
+ComponentD.contextType = UserContext
 
 export default ComponentD
